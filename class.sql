251e4fbe225f5714ba248ec33b1b6a8e47bf3697
TRamos5 <travis.ramos5@gmail.com>
2018-08-21 15:23:32 -0500

    project db exercise

--- class.sql
+++ class.sql
@@ -38,3 +38,74 @@
 -- );
 
 -- UPDATE student2 SET point = 8 WHERE id = 1;
+
+
+-- PostSQL Relationships Example
+
+-- CREATE TABLE author (
+--     id serial PRIMARY KEY,
+--     name VARCHAR
+-- );
+
+-- CREATE TABLE article (
+--     id serial PRIMARY KEY,
+--     title VARCHAR,
+--     author_id integer REFERENCES author (id)
+-- );
+
+-- INSERT INTO author VALUES (
+--     DEFAULT, 'Travis'
+-- );
+
+-- INSERT INTO author VALUES (
+--     DEFAULT, 'Matt'
+-- );
+
+-- INSERT INTO author VALUES (
+--     DEFAULT, 'Sam'
+-- );
+
+-- INSERT INTO author VALUES (
+--     DEFAULT, 'Tracy'
+-- );
+
+-- INSERT INTO author VALUES (
+--     DEFAULT, 'Eric'
+-- );
+
+-- INSERT INTO article VALUES (
+--     DEFAULT, 'Cats', 3
+-- );
+
+
+-- INSERT INTO article VALUES (
+--     DEFAULT, 'Dogs', 3
+-- );
+
+
+-- INSERT INTO article VALUES (
+--     DEFAULT, 'Birds', 3
+-- );
+
+
+-- INSERT INTO article VALUES (
+--     DEFAULT, 'Chickens', 3
+-- );
+
+
+-- INSERT INTO article VALUES (
+--     DEFAULT, 'Rabbits', 3
+-- );
+
+-- SELECT * FROM article WHERE author_id = 1;
+
+-- SELECT * FROM article, author 
+--     WHERE article.author_id = author.id and name = 'Sam';
+
+-- SELECT * FROM article 
+--     INNER JOIN author   
+--         on article.author_id = author.id;
+
+SELECT * FROM article
+    left outer join author  
+        on article.author_id = author.id;
